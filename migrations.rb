06ff086d67b1799cb977ca1eff52057cd6d79e3e
alishak82 <alisha82@gmail.com>
2014-10-14 17:20:31 -0400

    moved database info out of app and into separate folder so it wouldn't reload the database each time i started server

---
+++ migrations.rb
@@ -0,0 +1,12 @@
+require 'pg'
+
+conn = PG::Connection.open()
+conn.exec('DROP DATABASE IF EXISTS restaurant;')
+conn.exec('CREATE DATABASE restaurant;')
+conn.close
+
+conn = PG::Connection.open(dbname: 'restaurant')
+conn.exec('CREATE TABLE foods (id SERIAL PRIMARY KEY, name VARCHAR(100), cuisine VARCHAR(100), price INTEGER, description VARCHAR(255), allergens VARCHAR(100));')
+conn.exec('CREATE TABLE parties (id SERIAL PRIMARY KEY, name VARCHAR(50), size INTEGER, table_number INTEGER, has_paid boolean);')
+conn.exec('CREATE TABLE orders (id SERIAL PRIMARY KEY, parties_id INTEGER, foods_id INTEGER);')
+conn.close
